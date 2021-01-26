6bc97b7b2035783a594b0a42493ddaf5c9523f42
Albany <albany.prince@gmail.com>
2021-01-25 22:56:52 -0500

    homework

--- homework.js
+++ homework.js
@@ -1,37 +1,82 @@
 // 1. Set the variable `givenName` to the string "Addison".
+let givenName = 'Addison';
+
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+let candies = 20;
+let people = 6;
+let leftover = 20 % 6;
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+function greeting(name) {
+    return "Hello, " + name + "!"
+}
+
+
+
+
+
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
+function isOdd(num) {
+    if ((num % 2 === 1) || (num % 2 === -1)){
+    return true}
+    else {
+        return false
+    };
+}
+
 
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true}
+        else {return false};
+    }
+
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+function fahrenheitToCelsius(fTemp) {
+   //let fTemp = fahrenheit;
+   let cTemp = (fTemp - 32) * (5 / 9)
+   return cTemp;
 
+    
+}
 // 7. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
 
+function celsiusToFahrenheit(cTemp) {
+    let fTemp = (cTemp * 9 / 5) + 32
+    return fTemp;
+}
 // 8. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Kelvin. This function must use your previous
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
+function fahrenheitToKelvin (fTemp) {
+    let cTemp = (fTemp - 32) * (5 / 9)
+    let kTemp = cTemp + 273.15
+    return kTemp;
+}
+ 
+ 
 
 // 9. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+
 
 // 10. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
--- tests.js
+++ tests.js
@@ -4,6 +4,7 @@ describe('variables', function () {
   it('givenName should equal Addison', function () {
     assert.equal('Addison', givenName)
   })
+
   it('candies should equal 20', function () {
     assert.equal(20, candies)
   })
